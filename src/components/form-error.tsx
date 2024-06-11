fe43313102528493eda0686a1ec1af1342ec8899
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-11 22:57:59 +0530

    styles: change text size

--- src/components/form-error.tsx
+++ src/components/form-error.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FormError = ({ error }: { error: string }) => {
   return (
     error && (
-      <p className="bg-red-200 text-red-500 p-2 rounded border-red-600 border-2">
+      <p className="bg-red-200 text-red-500 p-2 rounded border-red-600 border-2 text-sm">
         {error}
       </p>
     )
--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FormSuccess = ({ success }: { success: string }) => {
   return (
     success && (
-      <p className="bg-emerald-200 text-emerald-500 p-2 rounded border-emerald-600 border-2">
+      <p className="bg-emerald-200 text-emerald-500 p-2 rounded border-emerald-600 border-2 text-sm">
         {success}
       </p>
     )
