7dedd64f08065f0e8ebd58891571ccd2f8c280e1
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-13 23:06:53 +0530

    fix: wrap (protected) routes with the `SessionProvider`

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,14 +1,17 @@
 import ProtectedNavbar from "@/components/protected/protected-navbar";
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col w-full min-h-screen">
-      <ProtectedNavbar />
-      <section className="flex-1 flex w-full justify-center mt-10 p-4 px-8 max-w-screen-xl mx-auto">
-        {children}
-      </section>
-    </main>
+    <SessionProvider>
+      <main className="flex flex-col w-full min-h-screen">
+        <ProtectedNavbar />
+        <section className="flex-1 flex w-full justify-center mt-10 p-4 px-8 max-w-screen-xl mx-auto">
+          {children}
+        </section>
+      </main>
+    </SessionProvider>
   );
 };
 
