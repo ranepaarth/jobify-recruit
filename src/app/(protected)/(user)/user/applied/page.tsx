3a09e3fbbacda61a39107174cc2d76e30d8e8700
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-13 14:58:47 +0530

    feat:🚧 user pages

---
+++ src/app/(protected)/(user)/user/applied/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppliedPage = () => {
+  return <div>AppliedPage</div>;
+};
+
+export default AppliedPage;
---
+++ src/app/(protected)/(user)/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <div className="flex flex-col w-full items-center">UserPage</div>;
+};
+
+export default UserPage;
