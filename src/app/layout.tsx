325d224a1bccc225170883114d5c76e338e9e3d4
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-13 14:58:14 +0530

    uploadthing `NextSSRPlugin`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { extractRouterConfig } from "uploadthing/server";
 import "./globals.css";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
+        {children}
+      </body>
     </html>
   );
 }
