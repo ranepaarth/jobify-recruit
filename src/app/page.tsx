318215917936697fa1f20b4f9ed8eefda031cf7d
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-15 21:19:51 +0530

    refactor: direct login
    If valid cookie is already present, then on the basis of the user role give access to specific routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import React from "react";
 
 const Home = async () => {
   const user = await getUser();
-  if (user.role === "USER") {
+  if (user?.role === "USER") {
     redirect("/user");
   }
-  if (user.role === "ADMIN") {
+  if (user?.role === "ADMIN") {
     redirect("/admin");
   }
-  
+
   return (
     <main className="w-full max-w-screen-lg mx-auto py-8 px-4 min-h-screen flex flex-col">
       <h2 className="text-2xl font-extrabold text-neutral-800 md:text-4xl">
