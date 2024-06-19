fee96f4569bd57d78cd88f59b986f20cf738f2af
ranepaarth <2019paarth.rane@ves.ac.in>
2024-06-19 23:55:42 +0530

    styles: apply padding to `auth-layout`

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -15,7 +15,7 @@ const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
     <main className="flex flex-col flex-1 min-h-screen">
       <Navbar />
       <section className="flex-1 flex items-center justify-center max-w-screen-xl mx-auto w-full px-4">
-        <div className="w-full max-w-[500px]">{children}</div>
+        <div className="w-full max-w-[500px] p-4">{children}</div>
       </section>
     </main>
   );
